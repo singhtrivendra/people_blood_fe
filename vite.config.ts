84c6b7caa124bc2fd250fece4a79602dcd1d26db
Trivendra_Singh <trivendrasingh0711@gmail.com>
2025-05-09 04:42:21 +0530

    added be url

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,31 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { componentTagger } from 'lovable-tagger';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    host: "::",
+    port: 8080,
+    proxy: {
+      '/api': {
+        // target: 'http://localhost:3000',
+        target: 'https://people-blood-be.onrender.com',
+        changeOrigin: true,
+      },
     },
   },
 }));
